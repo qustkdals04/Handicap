a34f7bac43b22878a10a822d19e85b19bbfcefa5
lee <sangeg12@daum.net>
2015-10-26 18:21:06 +0900

    종상-messagevo

---
+++ Handicap/src/com/handicap/model/beans/MessageVO.java
@@ -0,0 +1,48 @@
+package com.handicap.model.beans;
+
+public class MessageVO {
+	int messageno;
+	String sender;
+	String recipient;
+	String contents;
+	String date;
+	int status;
+	public int getMessageno() {
+		return messageno;
+	}
+	public void setMessageno(int messageno) {
+		this.messageno = messageno;
+	}
+	public String getSender() {
+		return sender;
+	}
+	public void setSender(String sender) {
+		this.sender = sender;
+	}
+	public String getRecipient() {
+		return recipient;
+	}
+	public void setRecipient(String recipient) {
+		this.recipient = recipient;
+	}
+	public String getContents() {
+		return contents;
+	}
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	
+	
+}
