617b47fc7664700ad0764d44ae2f102a911af808
qustkdals04 <qustkdals04@naver.com>
2015-11-10 10:55:55 +0900

    fsdafdsafasdfsadfsda

--- Handicap/src/com/handicap/control/BbsController.java
+++ Handicap/src/com/handicap/control/BbsController.java
@@ -51,9 +51,8 @@ public class BbsController {
 		// 먼저 파일 업로드, 폴더는 미리 만드세요
 		// 스프링의 MultipartFile을 이용한 업로드
 		List<MultipartFile> files = fv.getFiles();
-		Map map = new HashMap();		
-		/*String bbsflag = req.getParameter("bbsflag");*/
-		System.out.println(bbsflag);
+		Map map = new HashMap();	
+		
 		//boardService.insertBoard(uploadForm);
 		if(bbsflag.equals("bbsNoticeWrite")){					
 				if(bd.insert(bv)){
